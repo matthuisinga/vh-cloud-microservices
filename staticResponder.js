3ecb00842b0829dc5c66b311c1ada583182be3aa
Matt Huisinga <mhuisinga@ptc.com>
2016-03-24 11:53:04 -0500

    Static responder updates for relative path and port

--- staticResponder.js
+++ staticResponder.js
@@ -1,20 +1,33 @@
 var http = require('http'),
     fs = require('fs'),
+    mime = require('node-mime'),
+    process = require('process'),
     path = require('path');
 
-const PORT=8080
+const PORT=process.argv[2] || 8080;
+const docBase = process.argv[3] || "static";
 
+function makeAbsolute(pathStr) {
+	var absolute = pathStr;
+	if (!path.isAbsolute(pathStr)) {
+		absolute = path.join(__dirname, pathStr);
+	}
+	return absolute;
+}
 function handleRequest(request, response) {
-	console.log("it works!! Path was: " + request.url);
-	var filePath = path.join(__dirname, "static", request.url);
+	
+	var filePath = makeAbsolute(path.join(docBase, request.url));
 	var stat = fs.stat(filePath, function (error, stat) {
 		if (error) {
-			console.log("not found: " + request.url);
+			console.log("not found: " + request.url + " in " + filePath);
 			response.writeHead(404);
 			response.end();
 		} else {
+			console.log("Found Path: " + filePath);
+			var mimeType = mime.lookUpType(filePath.split(".").pop());
+			console.log("Determined Mime Type: " + mimeType);
 			response.writeHead(200, {
-				'Content-Type': "text/html",
+				'Content-Type': mimeType,
 				'Content-Length': stat.size
 			});
 
